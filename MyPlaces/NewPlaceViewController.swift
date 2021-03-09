9857544e9c77e5e37163e6dfe384ad86919ac421
n1kitich <76942708+n1kitich@users.noreply.github.com>
2021-03-09 18:44:03 +0200

    Image Selection

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class NewPlaceViewController: UITableViewController {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,15 +56,24 @@ extension NewPlaceViewController: UITextFieldDelegate {
 
 // MARK: - Work with image
 
-extension NewPlaceViewController {
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func chooseImagePicker(source: UIImagePickerController.SourceType) {
         
         if UIImagePickerController.isSourceTypeAvailable(source) {
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self     //делегирует загрузку картинки
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
         }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
     }
 }
